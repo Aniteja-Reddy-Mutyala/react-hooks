4860a9ed48c308f5ac5a8d8bbf4d703078a7b85a
Aniteja Reddy Mutyala <aniteja.reddy@gmail.com>
2025-10-06 17:26:24 -0700

    Data fetching with useFetch hook

--- src/index.js
+++ src/index.js
@@ -9,26 +9,29 @@ import React, {
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import { useInput } from "./useInput";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-const TreesContext = createContext();
-export const useTrees=()=>
-  useContext(TreesContext)
-const trees = [
-  { id: "1", type: "Maple" },
-  { id: "2", type: "Oak" },
-  { id: "3", type: "Family" },
-  { id: "4", type: "Component" },
-];
+import { useFetch } from "./useFetch";
 
+function App({login}){
+const {loading,data,error}=useFetch(`https://api.github.com/users/${login}`);
+if(loading){
+  return(
+    <h1>Loading</h1>
+  )
+}
+  if(error){
+    return <pre>{JSON.stringify(error,null,2)}</pre>
+  }
+  return(
+    <>
+    <pre>{JSON.stringify(data,null,2)}</pre>
+    </>
+  )
+}
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <TreesContext.Provider value={{ trees }}>
-    <App />
-  </TreesContext.Provider>
+  <App  login="Aniteja-reddy-mutyala"/>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
